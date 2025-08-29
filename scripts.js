39ff6b8e705356b7b8dd25cc4f208b482e8fc665
Roseane Laia <roseanelaia@hotmail.com>
2025-08-29 18:07:22 -0300

    adicionando o valor da despesa

--- scripts.js
+++ scripts.js
@@ -74,11 +74,19 @@ function expenseAdd(newExpense) {
         const expenseCategory = document.createElement("span")
         expenseCategory.textContent = newExpense.category_name
 
+
         // Adiciona nome e categoria na div das informações da despesa
         expenseInfo.append(expenseName, expenseCategory)
 
+        // Cria o valor da despesa.
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+            .toUpperCase()
+            .replace("R$", "")}` //como a própria span já traz o R$, por isso vai tirar o R$ do valor formatado utiizando o replace
+
         // Adiciona as informações do ícone no item.
-        expenseItem.append(expenseIcon, expenseInfo)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
 
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
