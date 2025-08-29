d4b662af92d5d76cb21b3662fd7cf2b88edf9a53
Roseane Laia <roseanelaia@hotmail.com>
2025-08-29 13:40:46 -0300

    formatando valores do input amount

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// Seleciona os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não númericos.
+    let value = amount.value.replace(/\D/g, "")
+
+    // Transformar o valor em centavos (exemplo: 150/100 = 1.50 que é equivalente a R$1,50).
+    value = Number(value) / 100
+
+    // Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value)
+} 
+
+function formatCurrencyBRL (value) {
+    // Formata o valor no padrão BRL (Real Brasileiro).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    
+    // Retorna o valor formatado
+    return value
+}
+
+// Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (event) => {
+    // rPrevine o comportamento padrão de recarregar a página.
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes na nova despesa.
+    const newExpense = {
+        id: new Date().getTime(), 
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+}
