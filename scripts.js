813d79118ebe0984763e53288ae2a516167ee617
Roseane Laia <roseanelaia@hotmail.com>
2025-08-29 19:17:08 -0300

    limapndo o formulario após adicionar um item

--- scripts.js
+++ scripts.js
@@ -99,6 +99,9 @@ function expenseAdd(newExpense) {
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
 
+        // Limpa o formulário para adicionar um novo item.
+        formClear()
+
         // Atualiza os totais.
         updateTotals()
 
@@ -174,3 +177,14 @@ expenseList.addEventListener("click", function (event) { // observa click em qua
     // Atualiza os totais.
     updateTotals()
 })
+
+function formClear() {
+    // Limpa os inputs.
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+
+    // Coloca o foco no input de amount.
+    expense.focus()
+
+}
