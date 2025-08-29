11b964e038665d36c7c0b057629565d14ecda961
Roseane Laia <roseanelaia@hotmail.com>
2025-08-29 17:56:36 -0300

    adicionando div

--- scripts.js
+++ scripts.js
@@ -62,8 +62,23 @@ function expenseAdd(newExpense) {
         expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
         expenseIcon.setAttribute("alt", newExpense.category_name)
 
+        // Cria as informações da despesa(div).
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        // Cria o nome da despesa.
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        // Cria a categoria da despesa.
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        // Adiciona nome e categoria na div das informações da despesa
+        expenseInfo.append(expenseName, expenseCategory)
+
         // Adiciona as informações do ícone no item.
-        expenseItem.append(expenseIcon)
+        expenseItem.append(expenseIcon, expenseInfo)
 
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
