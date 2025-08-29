3ba0659ea1dffa5af885acb2df3dc2e004c9a8b6
Roseane Laia <roseanelaia@hotmail.com>
2025-08-29 18:45:58 -0300

    calculando o valor total

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const category = document.getElementById("category")
 // Seleciona os elementos da lista.
 const expenseList = document.querySelector("ul")
 const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -116,6 +117,30 @@ function updateTotals () {
 
         // Atualiza a quantidade de itens da lista.
         expensesQuantity.textContent = `${items.length} ${items.length > 1? "despesas" : "despesa"}` //verificação para aparecer texto no plural ou singular
+
+        // Variável para incrementar o total.
+        let total = 0
+
+        // Percorre cada item (li) da lista (ul)
+        for(let item = 0;  item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+            
+            //Remover caracteres não númericos e substitui vírgula pelo ponto.
+            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",",".")
+
+            // Converte o valor para float.
+            value = parseFloat(value)
+
+            // Verificar se é um número válido.
+            if(isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número")
+            }
+
+            // Incrementar o valor total (total = total + value) ou
+            total += Number(value)
+        }
+
+        expensesTotal.textContent = total
 
     } catch (error) {
         console.log(error)
